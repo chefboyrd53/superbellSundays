af2a24f1ad817019ae9a7845e3ad7edc0e1231cd
chefboyrd53 <chefboyrd53@gmail.com>
2025-01-18 03:13:50 -0500

    fixed images

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files from the 'public' folder
 app.use(express.static('public'));
 
+// Serve static files from the 'assets/images' folder
+app.use('/assets/images', express.static('assets/images'));
+
+// Default route
 app.get('/', (req, res) => {
   res.send('Hello, Mario Kart Challenge!');
 });
